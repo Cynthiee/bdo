64add45cf7979732b252e81acc0e8cdfc45de29d
Cynthiee <islacynthia7@gmail.com>
2025-04-14 03:14:13 +0100

    host mng

--- bdobank/settings.py
+++ bdobank/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-z$mwwxiqk9o_3d9_mw^4o9_kj(_=(&_%8!xs9msshq_iojy@37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bdo-unibank.onrender.com']
 
 
 # Application definition
