5a442629eed7fc460fd07aa387c1722803eefb40
Cynthiee <islacynthia7@gmail.com>
2025-04-14 03:16:48 +0100

    whitenoise

--- bdobank/settings.py
+++ bdobank/settings.py
@@ -48,7 +48,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'bdobank.urls'
 
