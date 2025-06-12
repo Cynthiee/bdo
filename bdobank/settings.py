65738b8c0515cf7473480b5f646d6806e4b147ee
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-06-12 15:03:24 +0100

    stmp test 11

--- bdobank/settings.py
+++ bdobank/settings.py
@@ -130,14 +130,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # settings.py
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'ibeawuchichukwugozirim@gmail.com'
-EMAIL_HOST_PASSWORD = 'oqfbjvohqnwnvhqf'
-DEFAULT_FROM_EMAIL = 'ibeawuchichukwugozirim@gmail.com'
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'mainwesthern@gmail.com'
+EMAIL_HOST_PASSWORD = 'shdwegpvcxowwtiy'  # No spaces!
+DEFAULT_FROM_EMAIL = 'mainwesthern@gmail.com'
 
 
 CELERY_BROKER_URL = 'redis://red-d14ojb24d50c73cidhsg:6379/0'
